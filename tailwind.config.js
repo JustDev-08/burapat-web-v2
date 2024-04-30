9afe60b0f26aa8cd27dcf05f756ead541dfe5a64
Burapat Nun <93272624+JustDev-08@users.noreply.github.com>
2024-04-30 15:07:37 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        typewriter: "typewriter 1s steps(50) forwards"
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: "100%"
+          }
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
